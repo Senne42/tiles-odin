e452f58efee3a5a1c3465c2bc85e4e577d6f507d
Senne42 <mateussenne@hotmail.com>
2023-08-04 14:11:14 +1000

    paintTile implemented.

--- script.js
+++ script.js
@@ -2,17 +2,47 @@ document.addEventListener("DOMContentLoaded", function(){
     const slider = document.getElementById('gridSize')
     const sliderNumber = document.getElementById('sliderNumber')
     const tiles = document.getElementById('tiles')
+    
+    const selectedValue = slider.value;
+    fillGrid(selectedValue)
+    tiles.style.gridTemplateColumns = (`repeat(${selectedValue}, ${selectedValue}fr)`)
 
     slider.addEventListener('input', function(){
         const selectedValue = slider.value;
         sliderNumber.innerText = `${selectedValue} x ${selectedValue}`
         tiles.style.gridTemplateColumns = (`repeat(${selectedValue}, ${selectedValue}fr)`)
         fillGrid(selectedValue)
+        const tilePaint = Array.from(document.querySelectorAll('.tileTeste'))
+        tilePaint.forEach(box => box.addEventListener('mouseover', paintTile))
     })
-    cleanGrid()
-    fillGrid(slider.value)
-
+    
+    
 })
+
+function paintTile (e) {
+    const boxId = e.target.id;
+    const box = document.getElementById(boxId)
+    paint(box)
+    
+}
+
+
+function paint(box){
+    let brushColor = document.getElementById('brushColor')
+    let newopctValue = 10
+    box.setAttribute('style', `background-color: ${brushColor.value}; opacity: ${newopctValue}%;`)
+    let opctValueArray = box.getAttribute('style').split(" ")
+    let opctValue = opctValueArray[opctValueArray.length - 1]
+    let value =  parseInt(opctValue.slice(0, -2))
+    
+    if (value < 100) {
+        value = value + 10
+        console.log(value)
+    }
+
+    console.log(value)
+    console.log(typeof(opctValue))
+}
 
 function cleanGrid () {
     while (tiles.firstChild) {
@@ -26,6 +56,7 @@ function fillGrid (size) {
     for (let a=1; a <= size; a++) {       
         const box = document.createElement('div');
         box.setAttribute('class', 'tileTeste')
+        box.setAttribute('id', `box${a}`)
         tiles.appendChild(box)
     }
 }
@@ -43,4 +74,5 @@ repetir X vezes
 /*
 Mexer no slider zera os tiles
 botao reset
+arrumar tiles iniciais
 */
